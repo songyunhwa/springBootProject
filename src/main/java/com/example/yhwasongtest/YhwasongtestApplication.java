67bd813e670d32131c2ecf8da9449e64416d52da
unknown <happy0155@naver.com>
2021-09-03 21:49:44 +0900

    서버 이전 관련 설정 - 현재 디비 복원까지 완료

--- src/main/java/com/example/yhwasongtest/YhwasongtestApplication.java
+++ src/main/java/com/example/yhwasongtest/YhwasongtestApplication.java
@@ -4,12 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+import java.util.Collections;
+
 @SpringBootApplication
 @EnableRedisHttpSession
 public class YhwasongtestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YhwasongtestApplication.class, args);
+        SpringApplication app = new SpringApplication(YhwasongtestApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "9000"));
+        app.run(args);
+
+        //SpringApplication.run(YhwasongtestApplication.class, args);
     }
 
 }
