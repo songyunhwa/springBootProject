8df6641800e5475a9174822354928e7fdb46ef6a
songyunhwa <happy0155@naver.com>
2021-10-07 15:02:48 +0900

    서버관련 수정 - 레디스 ,파일

--- web/vue/src/components/youtube/Home.vue
+++ web/vue/src/components/youtube/Home.vue
@@ -16,7 +16,9 @@
 
     <!-- 하단 스크롤 바 -->
     <div class="scoll-menu">
-        <img  :src="image_path + 'scroll_up.png'" v-on:click="scrollUp"/>
+        <img
+              :src="require(`@/assets/images/scroll_up.png`)"
+              v-on:click="scrollUp"/>
     </div>
 </template>
 
@@ -32,7 +34,6 @@
             username: '',
             password: '',
             url: '',
-            image_path : '',
             input: '', // 입력하는 값
             msg: '',   // 실질적으로 넘어가는 값
             views: '0', // 하루 접속량,
@@ -40,7 +41,6 @@
         created() {
             this.username = this.$cookies.get('username');
             this.url = this.resourceHost + '/history';
-            this.image_path = this.imagePath;
         },
         methods: {
             scrollUp() {
